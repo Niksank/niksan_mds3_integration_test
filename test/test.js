726a18f1ae8d3adbfe01d3321a3c71ca9491c21a
= <=>
2019-06-07 14:28:04 +0200

    test.js completed

--- test/test.js
+++ test/test.js
@@ -24,3 +24,65 @@ const getCurrentCulor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
 // <-- FIN
+
+
+describe('List', () => {
+
+	// Get all color list
+  describe('/GET colors', () => {
+    it('​should return all colors​', (done) => {
+      chai.request(app).get('/colors').end((err, res) => {
+        expect(res).to.have.status(200);
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.results.should.be.a('array');
+        res.should.be.json;
+        res.body.results.length.should.be.eql(3);
+        done();
+      });
+    });
+  });
+  // invalid path
+  describe('/GET colors', () => {
+    it('should return Bad Request​', (done) => {
+      chai.request(app).get('/color').end((err, res) => {
+        expect(res).to.have.status(404);
+        res.should.have.status(404);
+        done();
+      });
+    });
+  });
+
+  //add color 
+  describe('/POST colors', () => {
+    it('should add new color', (done) => {
+      const param = {
+        color: 'BLACK',
+      };
+      chai.request(app).post('/colors').send(param).end((err, res) => {
+        expect(res).to.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object'); 
+        res.body.results.should.be.a('array');
+        res.body.results[3].should.be.equal(param.color);
+        done();
+      });
+    });
+  });
+
+	// Get new color list 
+  describe('/GET colors', () => {
+    it('​should return new color list Request​', (done) => {
+      chai.request(app).get('/colors').end((err, res) => {
+        expect(res).to.have.status(200);
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.results.should.be.a('array');
+        res.should.be.json;
+        res.body.results.length.should.be.equal(4);
+        done();
+      });
+    });
+  });	
+  
+ });
